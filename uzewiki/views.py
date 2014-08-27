b0b4a6a101e84324ea88265aee11a6ebaa0529e3
Richard Terry <git@radiac.net>
2014-08-27 12:42:17 +0100

    Fixed search; improved layout of edit page

--- uzewiki/views.py
+++ uzewiki/views.py
@@ -100,7 +100,7 @@ def search(request, wiki, wiki_slug):
     else:
         pages = perform_search(
             query,
-            models.Page.objects,
+            models.Page,
             ['title', 'content'],
         )
     
