a787b0a4c2f6524e878c34be2b5c4d6399e0e412
Richard Terry <git@radiac.net>
2018-05-26 15:54:05 +0100

    Updated urls for dj1.10

--- uzewiki/urls.py
+++ uzewiki/urls.py
@@ -1,66 +1,102 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 
 from uzewiki import settings
+from uzewiki import views
 
 if settings.SINGLE:
     url_dict = {
         'wiki_slug': settings.SINGLE_SLUG,
     }
-    
-    urlpatterns = patterns('uzewiki.views',
-        url(r'^(?P<page_slug>[-\w]+(/[-\w]+)*)/:edit/$', 'edit', url_dict,
+
+    urlpatterns = [
+        url(
+            r'^(?P<page_slug>[-\w]+(/[-\w]+)*)/:edit/$',
+            views.edit,
+            url_dict,
             name='uzewiki-edit',
         ),
-        url(r'^(?P<page_slug>[-\w]+(/[-\w]+)*)/$', 'show', url_dict,
+        url(
+            r'^(?P<page_slug>[-\w]+(/[-\w]+)*)/$',
+            views.show,
+            url_dict,
             name='uzewiki-show',
         ),
-        url(r'^:import/$', 'wiki_import', url_dict,
+        url(
+            r'^:import/$',
+            views.wiki_import,
+            url_dict,
             name='uzewiki-import',
         ),
-        url(r'^:search/$', 'search', url_dict,
+        url(
+            r'^:search/$',
+            views.search,
+            url_dict,
             name='uzewiki-search',
         ),
-        url(r'^:asset/(?P<asset_name>[-\w]+)/$', 'asset_details', url_dict,
+        url(
+            r'^:asset/(?P<asset_name>[-\w]+)/$',
+            views.asset_details,
+            url_dict,
             name='uzewiki-asset',
         ),
-        url(r'^:asset/(?P<asset_name>[-\w]+)/edit/$', 'asset_edit', url_dict,
+        url(
+            r'^:asset/(?P<asset_name>[-\w]+)/edit/$',
+            views.asset_edit,
+            url_dict,
             name='uzewiki-asset-edit',
         ),
-        url(r'^$', 'show', url_dict,
+        url(r'^$',
+            views.show,
+            url_dict,
             name='uzewiki-show',
         ),
-        url(r'^$', 'show', url_dict,
+        url(
+            r'^$',
+            views.show,
+            url_dict,
             name='uzewiki-index',
         ),
-    )
+    ]
 else:
-    urlpatterns = patterns('uzewiki.views',
-        url(r'^(?P<wiki_slug>[-\w]+)/(?P<page_slug>[-\w]+(/[-\w]+)*)/:edit/$',
-            'edit',
+    urlpatterns = [
+        url(
+            r'^(?P<wiki_slug>[-\w]+)/(?P<page_slug>[-\w]+(/[-\w]+)*)/:edit/$',
+            views.edit,
             name='uzewiki-edit',
         ),
-        url(r'^(?P<wiki_slug>[-\w]+)/(?P<page_slug>[-\w]+(/[-\w]+)*)/$',
-            'show',
+        url(
+            r'^(?P<wiki_slug>[-\w]+)/(?P<page_slug>[-\w]+(/[-\w]+)*)/$',
+            views.show,
             name='uzewiki-show'
         ),
-        url(r'^(?P<wiki_slug>[-\w]+)/$', 'show',
+        url(
+            r'^(?P<wiki_slug>[-\w]+)/$',
+            views.show,
             name='uzewiki-show'
         ),
-        url(r'^(?P<wiki_slug>[-\w]+)/:import/$', 'wiki_import',
+        url(
+            r'^(?P<wiki_slug>[-\w]+)/:import/$',
+            views.wiki_import,
             name='uzewiki-import',
         ),
-        url(r'^(?P<wiki_slug>[-\w]+)/:search/$', 'search',
+        url(
+            r'^(?P<wiki_slug>[-\w]+)/:search/$',
+            views.search,
             name='uzewiki-search',
         ),
-        url(r'^(?P<wiki_slug>[-\w]+)/:asset/(?P<asset_name>[-\w]+)/$',
-            'asset_details',
+        url(
+            r'^(?P<wiki_slug>[-\w]+)/:asset/(?P<asset_name>[-\w]+)/$',
+            views.asset_details,
             name='uzewiki-asset',
         ),
-        url(r'^(?P<wiki_slug>[-\w]+)/:asset/(?P<asset_name>[-\w]+)/edit/$',
-            'asset_edit',
+        url(
+            r'^(?P<wiki_slug>[-\w]+)/:asset/(?P<asset_name>[-\w]+)/edit/$',
+            views.asset_edit,
             name='uzewiki-asset-edit',
         ),
-        url(r'^$', 'index',
+        url(
+            r'^$',
+            views.index,
             name='uzewiki-index'
         ),
-    )
+    ]
