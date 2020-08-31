af67853a2103d911179da64c435013ce2309930d
Richard Terry <git@radiac.net>
2020-08-31 02:14:46 +0100

    Update packaging

--- setup.py
+++ setup.py
@@ -1,39 +1,16 @@
-import os
+import re
+from pathlib import Path
 
-from setuptools import find_packages, setup
-
-from powerwiki import __version__
-
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+from setuptools import setup
 
 
-setup(
-    name="django-powerwiki",
-    version=__version__,
-    author="Richard Terry",
-    author_email="code@radiac.net",
-    description=("A wiki system for Django"),
-    license="BSD",
-    url="http://radiac.net/projects/django-powerwiki/",
-    long_description=read("README.rst"),
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Environment :: Web Environment",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Framework :: Django",
-    ],
-    zip_safe=True,
-    install_requires=[
-        "Django>=2.2",
-        "django-yaa-settings",
-        "beautifulsoup4",
-        "docutils",
-    ],
-    packages=find_packages(),
-    include_package_data=True,
-)
+def find_version(*paths):
+    path = Path(*paths)
+    content = path.read_text()
+    match = re.search(r"^__version__\s*=\s*['\"]([^'\"]*)['\"]", content, re.M)
+    if match:
+        return match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+setup(version=find_version("powerwiki", "__init__.py"))
